107ba62053fedc93f4d4a691e837efdd7d37dd3c
gjainiitr <47331030+gjainiitr@users.noreply.github.com>
2021-07-25 12:31:48 +0530

    Configuring Redux, creating Store, Combining Reducer, using Provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import {Provider} from 'react-redux';
+import {configureStore, combineReducers} from "@reduxjs/toolkit";
+
+// Reducers
+import authReducer from './Controllers/Redux/authSlice';
+import bugReducer from './Controllers/Redux/bugSlice';
+import userReducer from './Controllers/Redux/userSlice';
+
+const reducer = combineReducers({
+  auth: authReducer,
+  bug: bugReducer,
+  user: userReducer
+});
+
+const store = configureStore({
+  reducer:reducer
+})
+
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
