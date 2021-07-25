1d62549180d77d3e25d161afaafd14ce173c6d32
gjainiitr <47331030+gjainiitr@users.noreply.github.com>
2021-07-25 12:14:22 +0530

    Created dummy data for bugs in Bug Controller

---
+++ frontend/src/Controllers/bugController.js
@@ -0,0 +1,32 @@
+import bugModel from '../Models/bugModel';
+
+export function retrieveBugs() {
+    let data = [];
+    
+    data.push(new bugModel({
+        _id:23456789,
+        name:"Crash on Load",
+        details:"Crashes after 3 seconds",
+        steps:"Open application and it will crash",
+        version:"V2.0",
+        assigned:"Gopal Jain",
+        created:"Hugh Mongus",
+        priority:1,
+        time:"19:35"
+    }));
+
+    data.push(new bugModel({
+        _id:23456754,
+        name:"Won't Load",
+        details:"Crashes after 3 seconds",
+        steps:"Open application and it will crash",
+        version:"V2.0",
+        assigned:"Gopal Jain",
+        created:"Hugh Mongus",
+        priority:1,
+        time:"19:35"
+    }));
+
+    let sorted = data.sort((a,b) => {return a.priority-b.priority}); 
+    return sorted;
+}
