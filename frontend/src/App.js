72d1f1299bee0dc9145ac7eb03b8a0a92303440a
gjainiitr <47331030+gjainiitr@users.noreply.github.com>
2021-07-25 18:39:31 +0530

    Setting up routing based on url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Login from './Views/Login/login';
 import Sidebar from './Views/sidebar/sidebar';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import ViewBugs from './Views/Pages/viewBugs';
 
 function App() {
@@ -13,7 +13,12 @@ function App() {
         !auth.loggedIn ? <Login /> : 
         <>
           <Sidebar/>
-          <ViewBugs/>
+          <Switch>
+            {/* <Route path="/"></Route> */}
+            <Route path="/viewbugs"><ViewBugs/></Route>
+            {/* <Route path="/create"></Route> */}
+          </Switch>
+          
         </>
       }
     </Router>
