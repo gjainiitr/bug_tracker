df2aa7cf2b7a80064a9305e66446443f90e591f4
gjainiitr <47331030+gjainiitr@users.noreply.github.com>
2021-07-25 17:25:16 +0530

    Sidebar changes, Partially adding ViewBugs
    ViewBugs is the display component which will house all the bugs.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Login from './Views/Login/login';
-
+import Sidebar from './Views/sidebar/sidebar';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   const {auth} = useSelector(state => state);
   return (
-    <>
+    <Router>
       {
-        !auth.loggedIn ? <Login /> : <></>
+        !auth.loggedIn ? <Login /> : <Sidebar />
       }
-    </>
+    </Router>
   );
 }
 
