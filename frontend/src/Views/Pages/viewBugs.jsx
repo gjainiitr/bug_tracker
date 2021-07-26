9d3dc8e6d177c9b6bfd6884425ba91b7ecdf65dc
gjainiitr <47331030+gjainiitr@users.noreply.github.com>
2021-07-26 10:30:18 +0530

    Props shown and unshown on clicks

--- frontend/src/Views/Pages/viewBugs.jsx
+++ frontend/src/Views/Pages/viewBugs.jsx
@@ -1,9 +1,15 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import {getBugs} from '../../Controllers/Redux/bugSlice'
 import BugCard from '../bugCard/bugCard'
+import BugDetailed from '../bugDetailed/bugDetailed'
 
 export default () => {
+    const [DISPLAY_BUG, SET_DISPLAY_BUG] = useState({
+        name:"",
+        isDisplayed:false
+    });
+    
     const dispatch = useDispatch();
     const {bugs} = useSelector(state => state);
     
@@ -13,16 +19,18 @@ export default () => {
     }, [bugs.length < 1])
 
     function bugClicked(name) {
-
+        SET_DISPLAY_BUG({
+            isDisplayed: !DISPLAY_BUG.isDisplayed,
+            name: name
+        });
     }
 
     return (
-        <div className='page-container'>
-            {
-                bugs.map((bug,key) => (
+        <div className='page-container'>            
+                {bugs.map((bug,key) => (
                     <BugCard key={key} bug={bug} clicked={bugClicked}/>
-                ))
-            }
+                ))}
+                {DISPLAY_BUG.isDisplayed && <BugDetailed bug={ bugs.filter((bug) => {return bug.name == DISPLAY_BUG.name})} />}            
         </div>
     )
 }
