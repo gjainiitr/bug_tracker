aa200351a5e71ef3521345eeb6ceb9247d4d765f
gjainiitr <47331030+gjainiitr@users.noreply.github.com>
2021-07-26 16:31:06 +0530

    Edit bug front-end complete

--- frontend/src/Views/bugDetailed/bugDetailed.jsx
+++ frontend/src/Views/bugDetailed/bugDetailed.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import BugDetailedSection from './Component/bugDetailedSection'
 import './bugDetailed.css'
 import bugModel from '../../Models/bugModel'
 import { useDispatch } from 'react-redux'
 import { markComplete } from '../../Controllers/Redux/bugSlice'
+import EditPanel from '../BugCreate/delete/editPanel'
+import EditBug from '../BugCreate/edit/bugForm'
 
 export default (props) => {
     const dispatch = useDispatch();
     const bug = new bugModel(props.bug[0]);
    
+    const [displayEdit, setDisplayEdit] = useState(false);
+
+    function editClicked() {
+        setDisplayEdit(!displayEdit);
+    }
+
+    function deleteClicked() {
+
+    }
+
     return(
+        <>
         <div className="bug-view">
+            <EditPanel editClicked={editClicked} deleteClicked={deleteClicked} />
             <button onClick={props.clicked} className='close-btn'>Close</button>
             <h1>{bug.name}</h1>
             <BugDetailedSection title="Details" info={bug.details} />
@@ -21,5 +35,7 @@ export default (props) => {
             <BugDetailedSection title="Time Created" info={bug.time} />
             <button onClick={() => {dispatch(markComplete())}}>Mark Complete</button>
         </div>
+        {displayEdit && <EditBug title="Edit Bug" bug={bug} close={editClicked} />}
+        </>
     )
 }
